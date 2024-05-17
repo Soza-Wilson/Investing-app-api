df7413d85adbda673be2af4857ac652dd670be7f
Wilson soza <84516302+Soza-Wilson@users.noreply.github.com>
2024-05-17 19:02:29 +0200

    feat: connecting to railways database

--- src/util/database.ts
+++ src/util/database.ts
@@ -2,7 +2,9 @@ import dotenv from 'dotenv'
 dotenv.config();
 import { Sequelize } from 'sequelize'
 
-const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USERNAME as string, process.env.DB_PASSWORD as string, { dialect: "mysql", host: process.env.DB_HOST as string })
+const port = process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined;
+
+const sequelize = new Sequelize(process.env.DB_NAME as string, process.env.DB_USERNAME as string, process.env.DB_PASSWORD as string, { dialect: "mysql", host: process.env.DB_HOST as string,port: port})
 
 
 export default sequelize;
